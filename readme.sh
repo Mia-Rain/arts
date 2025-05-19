b442ab98a2b3f0e67b3c2ea8abdc909174508a7d
Mia <mia-dl@tuta.io>
2025-05-19 05:59:14 -0400

    update

--- readme.sh
+++ readme.sh
@@ -4,7 +4,7 @@ printf '%s\narts\n%s\n' "---" "---"
 for i in ./*.*; do
   i="${i#./}"
   if [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
-    printf '://%s \n' "${i#./}"
+    printf '://%s\n' "${i#./}"
     : $((counter+=1))
   fi
 done
