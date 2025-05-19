31a4e7e99deb6c3c573dc46574f9effbd6211652
Mia <mia-dl@tuta.io>
2025-05-19 06:12:29 -0400

    fix link-list

--- link-list.sh
+++ link-list.sh
@@ -1,7 +1,7 @@
 counter=1
 for i in ./*.*; do
   i="${i#./}"
-  if [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] ; then
+  if [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
     printf '%s:./%s\n' "$counter" "${i#./}"
     : $((counter+=1))
   fi
